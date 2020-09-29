2f2f6da5dbe3500662959907c43d2bda57d3a591
tanmichael1 <michaelrstan@gmail.com>
2020-09-29 14:48:18 +1300

    Edit generated movie page, movieslist ranked by title

--- models/movie.js
+++ models/movie.js
@@ -1,20 +1,33 @@
 const movies = [];
 
-module.exports = class Movie{
-  constructor(id, title, director, genres, actors, runtime, release, summary, ageRating, price, poster, trailer) {
-      this.id = id;
-      this.title = title;
-      this.director = director;
-      this.genres = genres;
-      this.actors = actors;
-      this.runtime = runtime;
-      this.release = release;
-      this.summary = summary;
-      this.ageRating = ageRating;
-      this.price = price;
-      this.poster = poster;
-      this.trailer = trailer;
-    }
+module.exports = class Movie {
+  constructor(
+    id,
+    title,
+    director,
+    genres,
+    actors,
+    runtime,
+    release,
+    summary,
+    ageRating,
+    price,
+    poster,
+    trailer
+  ) {
+    this.id = id;
+    this.title = title;
+    this.director = director;
+    this.genres = genres;
+    this.actors = actors;
+    this.runtime = runtime;
+    this.release = release;
+    this.summary = summary;
+    this.ageRating = ageRating;
+    this.price = price;
+    this.poster = poster;
+    this.trailer = trailer;
+  }
 
   add() {
     movies.push(this);
@@ -22,18 +35,31 @@ module.exports = class Movie{
 
   static search(id) {
     var foundMovie = null;
-    movies.forEach(function(movie){
-			if(movie.id == id){
-				foundMovie = movie;
-			}
-		})
-		return foundMovie;
+    movies.forEach(function (movie) {
+      if (movie.id == id) {
+        foundMovie = movie;
+      }
+    });
+    return foundMovie;
   }
 
   static all() {
     return movies;
   }
-}
 
+  static allByTitle() {
+    return movies.sort(function (a, b) {
+      if (a.title < b.title) {
+        return -1;
+      }
+      if (a.title > b.title) {
+        return 1;
+      }
+      return 0;
+    });
+  }
 
-
+  order() {
+    movies.sort();
+  }
+};
