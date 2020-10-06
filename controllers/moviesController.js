5b6aa5e965c396f43b18012cecff05c6df964c67
prattjame <prattjame@ecs.vuw.ac.nz>
2020-10-06 21:59:20 +1300

    changes

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,9 +1,10 @@
 const Movie = require("../models/movie");
 const dbMovie = require("../dbmodels/Movie");
 
-/*Placeholder movies for testing without mongodb*/
-
 Movie.loadDB();
+
+/*
+Placeholder movies for testing without mongodb
 
 movie1 = new Movie(
   1,
@@ -59,34 +60,30 @@ movie3 = new Movie(
 movie1.add();
 movie2.add();
 movie3.add();
+*/
 
-exports.getDynamicMovieDetails = async (req, res, next) => {
-  Movie.loadDB();
+exports.getDynamicMovieDetails = (req, res, next) => {
   id = req.params.id;
   mov = Movie.search(id);
   res.render("movie", { pageTitle: mov.title, movie: mov });
 };
 
-exports.getMoviesPage = async (req, res, next) => {
-  //movieList = Movie.all();
+exports.getMoviesPage = (req, res, next) => {
   movieList = Movie.allByTitle();
   res.render("movieslist", { movies: movieList });
 };
 
-exports.getSearchPage = async (req, res, next) => {
-  //movieList = Movie.all();
+exports.getSearchPage = (req, res, next) => {
   searchList = Movie.allByTitle();
   res.render("search", { movies: searchList });
 };
 
-exports.getHomePage = async (req, res, next) => {
-  //movieList = Movie.all();
-  featuredMovie = movie3;
+exports.getHomePage = (req, res, next) => {
+  featuredMovie = Movie.search("5f7c0b617fbd2800a89ea326");
   res.render("home", { movie: featuredMovie });
 };
 
 exports.addMovie = async (req, res, next) => {
-  
   const dbmovie = new dbMovie({
     title: req.body.title,
     director: req.body.directors,
@@ -121,8 +118,61 @@ exports.addMovie = async (req, res, next) => {
     uploadedMovie.add();
 
     console.log('Added movie: ' + savedMovie.title);
-    res.redirect('upload');
+    res.redirect('/upload');
   } catch(err) {
     console.log('error');
   }
 };
+
+exports.removeMovie = async (req, res, next) => {
+  console.log(req.params.id);
+  const removedMovie = await dbMovie.deleteOne({_id: req.params.id});
+  await Movie.delete(req.params.id);
+  res.redirect('/movieslist');
+};
+
+exports.editMovie = async (req, res, next) => {
+  toEdit = Movie.search(req.params.id);
+
+  console.log(toEdit.id);
+
+  //DB update
+  await dbMovie.updateOne(
+    { _id: req.params.id }, 
+      { $set: {
+        title: req.body.newTitle,
+        director:req.body.newDirectors,
+        genres: req.body.newGenres,
+        actors: req.body.newActors,
+        runtime: req.body.newRuntime,
+        release: req.body.newYear,
+        summary: req.body.newSummary,
+        ageRating: req.body.newRating,
+        price: req.body.newPrice,
+        poster: req.body.newPoster,
+        trailer: req.body.newTrailer
+      }}
+  );
+
+  const editedMovie = await dbMovie.findById(req.params.id);
+
+  //Local update
+  toEdit.title = editedMovie.title;
+  toEdit.director = editedMovie.director;
+  toEdit.genres = editedMovie.genres;
+  toEdit.actors = editedMovie.actors;
+  toEdit.runtime = editedMovie.runtime;
+  toEdit.release = editedMovie.release;
+  toEdit.summary = editedMovie.summary;
+  toEdit.ageRating = editedMovie.ageRating;
+  toEdit.price = editedMovie.price;
+  toEdit.poster = editedMovie.poster;
+  toEdit.trailer = editedMovie.trailer;
+
+  console.log(toEdit.title);
+  
+  res.redirect('/movieslist/' + req.params.id);
+};
+
+
+
