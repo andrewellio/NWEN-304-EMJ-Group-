c22b44fdab1665a2a6636d2f301e03bd51acab14
Elliott Andrews <andrewelli@ecs.vuw.ac.nz>
2020-10-08 20:46:04 +1300

    Basic login working via console.log

--- website/routes/index.js
+++ website/routes/index.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var router = express.Router();
 var controller = require("../controllers/moviesController.js");
+var mongoose = require("mongoose");
 const User = require("../dbmodels/User");
+
 
 /* GET index page. */
 router.get("/", function (req, res, next) {
@@ -53,6 +55,49 @@ router.get("/login", function (req, res, next) {
   res.render("login", { title: "Express" });
 });
 
+var matchusr=false;
+var matchpsw=false;
+
+/* POST register page*/
+router.post("/login", function (req, res, next) {
+  const data = new User({
+    usr: req.body.usr,
+    psw: req.body.psw, 
+  });
+
+  User.findOne({ 'usr': req.body.usr}, 'usr', function (err, user) {
+    if(user.usr == req.body.usr){
+      matchusr=true;
+    }
+    if(err){
+      console.log("An error has occured while logging in")
+    }
+
+  });
+
+    User.findOne({ 'psw': req.body.psw}, 'psw', function (err, user) {
+      if(user.psw == req.body.psw){
+        matchpsw=true;
+      }
+      if(err){
+        console.log("An error has occured while logging in")
+      }
+    
+    });
+
+      if(matchusr && matchpsw){
+        console.log("Login successfill, welcome " + req.body.usr)
+      }
+      else{
+        console.log("Login was unsuccessful")
+      }
+      res.render("movies", { title: "Express" });
+    });
+
+  
+    //if user.usr equals req.body.usr and user.psw equals req.body.psw then create session cookie, update navbar, hello! <username> and 
+    //change routes to use users.js
+
 /* GET register page. */
 router.get("/register", function (req, res, next) {
   res.render("register", { title: "Express" });
@@ -67,7 +112,7 @@ router.post("/register", function (req, res, next) {
     psw: req.body.psw,
   });
   data.save();
-  //console.log(req.body);
+  res.render("login", { title: "Express" });
 });
 
 /* GET upload (admin) page. */
