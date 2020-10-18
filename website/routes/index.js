f1dccc4807bf515578a5f37771aea034bbcd595d
Elliott Andrews <andrewelli@ecs.vuw.ac.nz>
2020-10-18 20:20:14 +1300

    Facebook Login Implemented and Index

--- website/routes/index.js
+++ website/routes/index.js
@@ -96,4 +96,28 @@ function isLoggedIn(req, res, next){
   res.render("/login");
 }
 
+/* GET facebook page. */
+router.get("/facebook", function (req, res, next) {
+  res.render("facebook", { title: "Express" });
+});
+
+router.get("/facebookLog", function (req, res, next) {
+  res.render("myaccount", { name: 'Facebook User', email: 'User Email'})
+});
+
+router.get('/auth/facebook', passport.authenticate((['local', 'facebook']), {
+  scope: ['public_profile', 'email']
+}));
+
+router.get('/auth/facebook/callback',
+  passport.authenticate((['local', 'facebook']), {
+    successRedirect: '/facebookLog',
+    failureRedirect: '/error'
+  }));
+
+router.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
 module.exports = router;
