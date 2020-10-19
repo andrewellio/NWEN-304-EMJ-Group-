6612e45e5a1e77fe0692b12ba644e055b1a5fb3c
prattjame <prattjame@ecs.vuw.ac.nz>
2020-10-19 21:15:21 +1300

    test

--- routes/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	'facebookAuth': {
-		'clientID':  '3324486240965989',
-		'clientSecret':  '29b1fa401a24155bb581aec30dde9c99',
-		'callbackURL':  'http://localhost:3000/auth/facebook/callback'
-	}
-}
